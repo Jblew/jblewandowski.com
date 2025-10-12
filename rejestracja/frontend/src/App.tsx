9df3926b1a4a00bcba323a459fb1b4a6087287f1
Jędrzej Bogumił Lewandowski MD <jedrzejblew@gmail.com>
2025-10-12 20:19:15 +0200

    fix: click to enable

--- rejestracja/frontend/src/App.tsx
+++ rejestracja/frontend/src/App.tsx
@@ -4,12 +4,22 @@ import { Questionnaire, type ServiceSelection } from './Questionnaire';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { DiscountCodeSelection } from './DiscountCode';
 import { Schedule } from './Schedule';
+import  Button  from 'react-bootstrap/Button';
+import Stack from 'react-bootstrap/Stack';
 
 
 export function App() {
+    const [isEnabled, setEnabled] = useState<boolean>(false);
     const [email, setEmail] = useState<string | null>(null);
     const [discountPercent, setDiscountPercent] = useState(0);
     const [service, setService] = useState<ServiceSelection | null>(null);
+    if(!isEnabled) {
+        return (
+        <Stack>
+            <Button variant='primary' className='mx-auto' onClick={() => setEnabled(true)}>Przejdź do rejestracji</Button>
+        </Stack>
+        )
+    }
     return (
         <div>
             <Auth onEmailAvailable={setEmail} />
